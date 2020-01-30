bfe70652f810189cd09b50fcb932b5c7ef408ea8
Benjamin Cottrell <me@bgcottrell.com>
2020-01-30 16:10:55 +1300

    Some content and styling changes

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -8,9 +8,22 @@ class AboutMe extends React.Component {
 
                 <Card>
                     <CardBody >
-                        <CardTitle>Card title</CardTitle>
-                        <CardSubtitle>Card subtitle</CardSubtitle>
-                        <CardText>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitais nisi ut aliquip ex \sse cillum dolore eu fugiat nulla pariatur. Exce\".</CardText>
+                        <CardTitle>Welcome to my Ramblings</CardTitle>
+                        <CardSubtitle>Yep, that's right.</CardSubtitle>
+                        <CardText>On this website I ramble about all 
+                            manner of things including (but not limited to):
+                            <br/>
+                            <ul>
+                            <li>Tech</li>
+                            <li>Spicy food </li>
+                            <li>Memes</li>
+                            </ul>
+                            <br/>
+                            <small>NB: My ramblings are coherent. I have the right to move, 
+                                remove, dislocate and discombombulate any content that it's
+                                deemed fit to do so at my discretion. By continuing to view
+                                my website, you agree that this is just so.</small>
+                        </CardText>
                     </CardBody>
                 </Card>
 
--- src/Components/MainPage.tsx
+++ src/Components/MainPage.tsx
@@ -9,7 +9,8 @@ class MainPage extends React.Component {
                 <Row noGutters={false}>
                     <Col xs={12} sm={12} md={4} lg={4} xl={4}>
                         <Card>
-                            <CardImg src="https://media.licdn.com/dms/image/C4E03AQHYA7Jp9HFc5w/profile-displayphoto-shrink_200_200/0?e=1560988800&v=beta&t=akYMwrvYPCyFg7Miu6lY9SF-tQRRqxhzjlCmvF75sDQ" alt="Card image cap" />
+                            {/* <CardImg src="https://media.licdn.com/dms/image/C4E03AQHYA7Jp9HFc5w/profile-displayphoto-shrink_200_200/0?e=1560988800&v=beta&t=akYMwrvYPCyFg7Miu6lY9SF-tQRRqxhzjlCmvF75sDQ" alt="Card image cap" /> */}
+                            <CardImg src="https://avatars0.githubusercontent.com/u/19580905?s=460&v=4" alt="Profile Picture" />
                         </Card>
                     </Col>
                     <Col>
